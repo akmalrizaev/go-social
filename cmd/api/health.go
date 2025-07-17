975112cf0216ca3ed381c2e694bc8c717e28a69b
Akmal Rizaev <akmaljon.rizayev@gmail.com>
2025-07-17 16:51:38 +0500

    Implementing the Repository Pattern

--- cmd/api/health.go
+++ cmd/api/health.go
@@ -4,4 +4,6 @@ import "net/http"
 
 func (app *application) healthCheckHandler(w http.ResponseWriter, r *http.Request) {
 	w.Write(([]byte("welcome!")))
+
+	app.store.Posts.Create(r.Context())
 }
--- cmd/api/main.go
+++ cmd/api/main.go
@@ -4,14 +4,18 @@ import (
 	"log"
 
 	"github.com/akmalrizaev/go-social/internal/env"
+	"github.com/akmalrizaev/go-social/internal/store"
 )
 
 func main() {
 	cfg := config{
 		addr: env.GetString("ADDR", ":8080"),
 	}
+
+	store := store.NewStorage(nil)
 	app := &application{
 		config: cfg,
+		store:  store,
 	}
 
 	mux := app.mount()
